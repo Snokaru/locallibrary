359d303db9f1d926de3abec7ca56e3773547f7a7
vlad <vlad.slivilescu@gmail.com>
2020-07-01 19:36:43 +0300

    Add login logic

--- catalog/views.py
+++ catalog/views.py
@@ -2,18 +2,21 @@ from django.shortcuts import render
 from django.views import generic
 from .models import Book, Author, BookInstance, Genre, Language
 
-# Create your views here.
 def index(request):
     num_books = Book.objects.all().count() # The Number Of Books
     num_instances = BookInstance.objects.all().count()
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
     num_authors = Author.objects.count() # 
 
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_visits': num_visits,
     }
 
     return render(request, 'index.html', context=context)
@@ -24,3 +27,10 @@ class BookListView(generic.ListView):
 
 class BookDetailView(generic.DetailView):
     model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 10
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
